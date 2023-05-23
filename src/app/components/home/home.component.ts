3c65b91f578c659b17b3cb1087721f00e6f23970
Adrián Maza <>
2023-05-23 12:42:11 +0200

    angular basic example completed

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  name: string;
+  lastName: string;
+  message: string;
+  isDisabled: boolean;
+
+  constructor() {
+    this.name = "John";
+    this.lastName = "Doe";
+    this.message = "Enter your name";
+    this.isDisabled = false;
+  }
+
+  sayHello(): void {
+    console.log(`Hello ${this.name} ${this.lastName}`);    
+  }
+}
